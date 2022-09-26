6b15bfb323a90ba2b4f99f008a34683cffd1960e
ikkenzi <tavorearljohn@gmail.com>
2022-09-26 13:49:23 +0800

    impliment dynamic timer but still have errors

--- README.md
+++ README.md
@@ -2,3 +2,24 @@
 A discord bot that helps you study using
 the "Pomodoro" method
 -Dont fear PoMoHomoBro is here-
+#How to use
+**!start focus time | rest time | long rest time | number of sessions**
+**eg..**
+**time measurement is in minutes**
+**!start 30 5 60 4
+###where###
+- 30 is focus minutes/learning time
+- 5 is rest/break minutes
+- 60 is the long rest/break minutes
+- 4 is the number of rounds/session
+
+##Why no Pause option?##
+**altho PoMohomoBro is nice he can be strict specially on its duty 
+PoMoHomobro says-**  
+*"Pomodoro is made for us to have a focused learning time pausing and doing something or even an emergemcy will break the
+very concept of the POMODORO method"*
+
+#Tasklist for devs
+ - [x] build bare bones 
+ - [] dynamic input
+ - [] impliment stop
--- src/main.rs
+++ src/main.rs
@@ -17,44 +17,86 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        let active_session = Duration::new(10, 0);
-        let rest_session = Duration::new(5, 0);
-        if msg.content == "!start" {
-            let mut session: u8 = 0;
-            let start_response = MessageBuilder::new()
-                .push(&msg.author)
-                .push("active session starts..")
+        if msg.content.starts_with("!start") {
+            let show_guide = MessageBuilder::new()
+                .push_bold(
+                    "How to use\n
+                *time measurement is in minutes*\n
+                !start focus time | rest time | long rest time | number of sessions\n
+                !start 30 5 60 4\n
+                where\n
+                    - 30 is focus minutes/learning time\n
+                    - 5 is rest/break minutes\n
+                    - 60 is the long rest/break minutes\n
+                    - 4 is the number of rounds/session\n",
+                )
                 .build();
-            let rest_response = MessageBuilder::new()
-                .push(&msg.author)
-                .push(" Active session ends time to rest..")
-                .build();
-            let end_response = MessageBuilder::new()
-                .push(&msg.author)
-                .push(" End Session!")
-                .build();
-            //A basic and traditional pomodoro contains 4 iterations of the methods eg.. active-rest 1 | active-rest 2 | active-rest 3 | active-rest 4
-            while session != 4 {
-                if let Err(why) = msg.reply_ping(&ctx.http, &start_response).await {
-                    println!("Error sending message: {:?}", why);
+            let user_input = msg.content.strip_prefix("!start"); // remove start
+            let input_info: Vec<&str>;
+            match user_input {
+                Some(input) => {
+                    println!("{}", input);
+                    input_info = input.trim().split(' ').collect();
+                    //set the vectors value to respected value to build dynamic active[0]|rest[1]|long[2]|session[3]
+                    let focus_time: u32 = input_info[0].parse().unwrap();
+                    let rest_time: u32 = input_info[1].parse().unwrap();
+                    let long_rest_time: u32 = input_info[2].parse().unwrap();
+                    let number_sessions: u32 = input_info[3].parse().unwrap();
+                    let active_session = Duration::new((focus_time * 60) as u64, 0);
+                    let rest_session = Duration::new((rest_time * 60) as u64, 0);
+                    let long_rest_session = Duration::new((long_rest_time * 60) as u64, 0);
+                    //main process
+                    let mut session: u32 = 0;
+                    let start_response = MessageBuilder::new()
+                        .push(&msg.author)
+                        .push("active session starts..")
+                        .build();
+                    let rest_response = MessageBuilder::new()
+                        .push(&msg.author)
+                        .push(" Active session ends time to rest..")
+                        .build();
+                    let long_rest_response = MessageBuilder::new()
+                        .push(&msg.author)
+                        .push(" Session finished long rest..")
+                        .build();
+                    let end_response = MessageBuilder::new()
+                        .push(&msg.author)
+                        .push(" End Session!")
+                        .build();
+
+                    //pomodoro main process
+                    //A basic and traditional pomodoro contains 4 iterations of the methods eg.. active-rest 1 | active-rest 2 | active-rest 3 | active-rest 4
+                    while session != number_sessions {
+                        if let Err(why) = msg.reply_ping(&ctx.http, &start_response).await {
+                            println!("Error sending message: {:?}", why);
+                        }
+                        //timer for the current active/pomodoro session
+                        sleep(active_session).await;
+                        //timer for the rest_session
+                        if let Err(_) = msg.reply_ping(&ctx.http, &rest_response).await {
+                            println!("Error");
+                        }
+                        sleep(rest_session).await;
+                        session += 1;
+                        //end session
+                        if session == 4 {
+                            if let Err(_) = msg.reply_ping(&ctx.http, &long_rest_response).await {
+                                println!("Error");
+                            }
+                            sleep(long_rest_session).await;
+                            if let Err(_) = msg.reply_ping(&ctx.http, &end_response).await {
+                                println!("Error");
+                            }
+                        }
+                    } //while loop end
                 }
-                //timer for the current active/pomodoro session
-                sleep(active_session).await;
-                //timer for the rest_session
-                if let Err(_) = msg.reply_ping(&ctx.http, &rest_response).await {
-                    println!("Error");
-                }
-                sleep(rest_session).await;
-                session += 1;
-                //end session
-                if session == 4 {
-                    if let Err(_) = msg.reply_ping(&ctx.http, &end_response).await {
-                        println!("Error");
+                None => {
+                    if let Err(e) = msg.reply_mention(&ctx.http, &show_guide).await {
+                        println!("An error has occured log:{}", e);
                     }
                 }
-            } //while loop end
-        } //msg start
-
+            }
+        }
         if msg.content == "!stats" {
             if let Err(_) = msg.reply_ping(&ctx.http, "Under development").await {
                 println!("Error");
@@ -66,7 +108,6 @@ impl EventHandler for Handler {
     // shard is booted, and a READY payload is sent by Discord. This payload
     // contains data like the current user's guild Ids, current user data,
     // private channels, and more.
-    //
     // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
